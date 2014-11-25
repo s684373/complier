76e8dc97d7ba276f1582aa0c10f614c9edd673a7
rithd <775223480@qq.com>
2014-11-25 21:00:57 +0800

    llvm

--- qsort.s
+++ qsort.s
@@ -1,259 +1,194 @@
-	.file	"qsort.cpp"
-	.local	_ZStL8__ioinit
-	.comm	_ZStL8__ioinit,1,1
+	.file	"qsort.bc"
+	.section	.text.startup,"ax",@progbits
+	.align	16, 0x90
+	.type	__cxx_global_var_init,@function
+__cxx_global_var_init:                  # @__cxx_global_var_init
+	.cfi_startproc
+# BB#0:
+	pushq	%rax
+.Ltmp1:
+	.cfi_def_cfa_offset 16
+	movl	$_ZStL8__ioinit, %edi
+	callq	_ZNSt8ios_base4InitC1Ev
+	movl	$_ZNSt8ios_base4InitD1Ev, %edi
+	movl	$_ZStL8__ioinit, %esi
+	movl	$__dso_handle, %edx
+	callq	__cxa_atexit
+	popq	%rax
+	ret
+.Ltmp2:
+	.size	__cxx_global_var_init, .Ltmp2-__cxx_global_var_init
+	.cfi_endproc
+
 	.text
 	.globl	_Z6qusortiiPi
-	.type	_Z6qusortiiPi, @function
-_Z6qusortiiPi:
-.LFB1329:
+	.align	16, 0x90
+	.type	_Z6qusortiiPi,@function
+_Z6qusortiiPi:                          # @_Z6qusortiiPi
 	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$48, %rsp
-	movl	%edi, -36(%rbp)
-	movl	%esi, -40(%rbp)
-	movq	%rdx, -48(%rbp)
-	movl	-36(%rbp), %eax
-	cmpl	-40(%rbp), %eax
-	jl	.L2
-	jmp	.L1
-.L2:
-	movl	-36(%rbp), %eax
-	movl	%eax, -20(%rbp)
-	movl	-40(%rbp), %eax
-	movl	%eax, -16(%rbp)
-	movl	-20(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rdx, %rax
-	movl	(%rax), %eax
-	movl	%eax, -12(%rbp)
-	movl	-20(%rbp), %eax
-	movl	%eax, -28(%rbp)
-	movl	-20(%rbp), %eax
-	addl	$1, %eax
-	movl	%eax, -24(%rbp)
-	jmp	.L4
-.L6:
-	movl	-24(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rdx, %rax
-	movl	(%rax), %eax
-	cmpl	-12(%rbp), %eax
-	jge	.L5
-	addl	$1, -28(%rbp)
-	movl	-24(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rdx, %rax
-	movl	(%rax), %eax
-	movl	%eax, -8(%rbp)
-	movl	-24(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rax, %rdx
-	movl	-28(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rcx
-	movq	-48(%rbp), %rax
-	addq	%rcx, %rax
-	movl	(%rax), %eax
-	movl	%eax, (%rdx)
-	movl	-28(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rax, %rdx
-	movl	-8(%rbp), %eax
-	movl	%eax, (%rdx)
-.L5:
-	addl	$1, -24(%rbp)
-.L4:
-	movl	-24(%rbp), %eax
-	cmpl	-16(%rbp), %eax
-	jle	.L6
-	movl	-20(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rdx, %rax
-	movl	(%rax), %eax
-	movl	%eax, -4(%rbp)
-	movl	-20(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rax, %rdx
-	movl	-28(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rcx
-	movq	-48(%rbp), %rax
-	addq	%rcx, %rax
-	movl	(%rax), %eax
-	movl	%eax, (%rdx)
-	movl	-28(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rax, %rdx
-	movl	-4(%rbp), %eax
-	movl	%eax, (%rdx)
-	movl	-28(%rbp), %eax
-	leal	-1(%rax), %ecx
-	movq	-48(%rbp), %rdx
-	movl	-36(%rbp), %eax
-	movl	%ecx, %esi
-	movl	%eax, %edi
-	call	_Z6qusortiiPi
-	movl	-28(%rbp), %eax
-	leal	1(%rax), %ecx
-	movq	-48(%rbp), %rdx
-	movl	-40(%rbp), %eax
-	movl	%eax, %esi
-	movl	%ecx, %edi
-	call	_Z6qusortiiPi
-.L1:
-	leave
-	.cfi_def_cfa 7, 8
+# BB#0:
+	subq	$56, %rsp
+.Ltmp4:
+	.cfi_def_cfa_offset 64
+	movl	%edi, 52(%rsp)
+	movl	%esi, 48(%rsp)
+	movq	%rdx, 40(%rsp)
+	movl	52(%rsp), %eax
+	cmpl	48(%rsp), %eax
+	jge	.LBB1_7
+# BB#1:
+	movl	52(%rsp), %eax
+	movl	%eax, 36(%rsp)
+	movl	48(%rsp), %eax
+	movl	%eax, 32(%rsp)
+	movslq	36(%rsp), %rax
+	movq	40(%rsp), %rcx
+	movl	(%rcx,%rax,4), %eax
+	movl	%eax, 28(%rsp)
+	movl	36(%rsp), %eax
+	movl	%eax, 24(%rsp)
+	movl	36(%rsp), %eax
+	incl	%eax
+	movl	%eax, 20(%rsp)
+	jmp	.LBB1_2
+	.align	16, 0x90
+.LBB1_5:                                #   in Loop: Header=BB1_2 Depth=1
+	incl	20(%rsp)
+.LBB1_2:                                # =>This Inner Loop Header: Depth=1
+	movl	20(%rsp), %eax
+	cmpl	32(%rsp), %eax
+	jg	.LBB1_6
+# BB#3:                                 #   in Loop: Header=BB1_2 Depth=1
+	movslq	20(%rsp), %rax
+	movq	40(%rsp), %rcx
+	movl	(%rcx,%rax,4), %eax
+	cmpl	28(%rsp), %eax
+	jge	.LBB1_5
+# BB#4:                                 #   in Loop: Header=BB1_2 Depth=1
+	incl	24(%rsp)
+	movslq	20(%rsp), %rax
+	movq	40(%rsp), %rcx
+	movl	(%rcx,%rax,4), %eax
+	movl	%eax, 16(%rsp)
+	movslq	24(%rsp), %rcx
+	movq	40(%rsp), %rax
+	movl	(%rax,%rcx,4), %ecx
+	movslq	20(%rsp), %rdx
+	movl	%ecx, (%rax,%rdx,4)
+	movq	40(%rsp), %rax
+	movslq	24(%rsp), %rcx
+	movl	16(%rsp), %edx
+	movl	%edx, (%rax,%rcx,4)
+	jmp	.LBB1_5
+.LBB1_6:
+	movslq	36(%rsp), %rax
+	movq	40(%rsp), %rcx
+	movl	(%rcx,%rax,4), %eax
+	movl	%eax, 12(%rsp)
+	movslq	24(%rsp), %rcx
+	movq	40(%rsp), %rax
+	movl	(%rax,%rcx,4), %ecx
+	movslq	36(%rsp), %rdx
+	movl	%ecx, (%rax,%rdx,4)
+	movq	40(%rsp), %rax
+	movslq	24(%rsp), %rcx
+	movl	12(%rsp), %edx
+	movl	%edx, (%rax,%rcx,4)
+	movq	40(%rsp), %rdx
+	movl	52(%rsp), %edi
+	movl	24(%rsp), %esi
+	decl	%esi
+	callq	_Z6qusortiiPi
+	movl	24(%rsp), %edi
+	incl	%edi
+	movq	40(%rsp), %rdx
+	movl	48(%rsp), %esi
+	callq	_Z6qusortiiPi
+.LBB1_7:
+	addq	$56, %rsp
 	ret
+.Ltmp5:
+	.size	_Z6qusortiiPi, .Ltmp5-_Z6qusortiiPi
 	.cfi_endproc
-.LFE1329:
-	.size	_Z6qusortiiPi, .-_Z6qusortiiPi
-	.section	.rodata
-.LC0:
-	.string	" "
-	.text
+
 	.globl	main
-	.type	main, @function
-main:
-.LFB1330:
+	.align	16, 0x90
+	.type	main,@function
+main:                                   # @main
 	.cfi_startproc
-	.cfi_personality 0x3,__gxx_personality_v0
-	.cfi_lsda 0x3,.LLSDA1330
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$48, %rsp
-	movl	$14, -32(%rbp)
-	movl	$2, -28(%rbp)
-	movl	$56, -24(%rbp)
-	movl	$8, -20(%rbp)
-	movl	$6, -16(%rbp)
-	leaq	-32(%rbp), %rax
-	movq	%rax, %rdx
+# BB#0:
+	subq	$40, %rsp
+.Ltmp7:
+	.cfi_def_cfa_offset 48
+	movl	$0, 36(%rsp)
+	movl	.L_ZZ4mainE1a+16(%rip), %eax
+	movl	%eax, 32(%rsp)
+	vmovaps	.L_ZZ4mainE1a(%rip), %xmm0
+	vmovaps	%xmm0, 16(%rsp)
+	leaq	16(%rsp), %rdx
+	xorl	%edi, %edi
 	movl	$4, %esi
-	movl	$0, %edi
-.LEHB0:
-	call	_Z6qusortiiPi
-	movl	$0, -36(%rbp)
-	jmp	.L8
-.L9:
-	movl	-36(%rbp), %eax
-	cltq
-	movl	-32(%rbp,%rax,4), %eax
-	movl	%eax, %esi
+	callq	_Z6qusortiiPi
+	movl	$0, 12(%rsp)
+	jmp	.LBB2_1
+	.align	16, 0x90
+.LBB2_2:                                #   in Loop: Header=BB2_1 Depth=1
+	movslq	12(%rsp), %rax
+	movl	16(%rsp,%rax,4), %esi
 	movl	$_ZSt4cout, %edi
-	call	_ZNSolsEi
-	movl	$.LC0, %esi
+	callq	_ZNSolsEi
 	movq	%rax, %rdi
-	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
-.LEHE0:
-	addl	$1, -36(%rbp)
-.L8:
-	cmpl	$4, -36(%rbp)
-	jle	.L9
-	movl	$0, %eax
-	jmp	.L13
-.L12:
-	movq	%rax, %rdi
-.LEHB1:
-	call	_Unwind_Resume
-.LEHE1:
-.L13:
-	leave
-	.cfi_def_cfa 7, 8
+	movl	$.L.str, %esi
+	callq	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	incl	12(%rsp)
+.LBB2_1:                                # =>This Inner Loop Header: Depth=1
+	cmpl	$4, 12(%rsp)
+	jle	.LBB2_2
+# BB#3:
+	movl	36(%rsp), %eax
+	addq	$40, %rsp
 	ret
+.Ltmp8:
+	.size	main, .Ltmp8-main
 	.cfi_endproc
-.LFE1330:
-	.globl	__gxx_personality_v0
-	.section	.gcc_except_table,"a",@progbits
-.LLSDA1330:
-	.byte	0xff
-	.byte	0xff
-	.byte	0x1
-	.uleb128 .LLSDACSE1330-.LLSDACSB1330
-.LLSDACSB1330:
-	.uleb128 .LEHB0-.LFB1330
-	.uleb128 .LEHE0-.LEHB0
-	.uleb128 .L12-.LFB1330
-	.uleb128 0
-	.uleb128 .LEHB1-.LFB1330
-	.uleb128 .LEHE1-.LEHB1
-	.uleb128 0
-	.uleb128 0
-.LLSDACSE1330:
-	.text
-	.size	main, .-main
-	.type	_Z41__static_initialization_and_destruction_0ii, @function
-_Z41__static_initialization_and_destruction_0ii:
-.LFB1338:
+
+	.section	.text.startup,"ax",@progbits
+	.align	16, 0x90
+	.type	_GLOBAL__I_a,@function
+_GLOBAL__I_a:                           # @_GLOBAL__I_a
 	.cfi_startproc
-	pushq	%rbp
+# BB#0:
+	pushq	%rax
+.Ltmp10:
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	cmpl	$1, -4(%rbp)
-	jne	.L14
-	cmpl	$65535, -8(%rbp)
-	jne	.L14
-	movl	$_ZStL8__ioinit, %edi
-	call	_ZNSt8ios_base4InitC1Ev
-	movl	$__dso_handle, %edx
-	movl	$_ZStL8__ioinit, %esi
-	movl	$_ZNSt8ios_base4InitD1Ev, %edi
-	call	__cxa_atexit
-.L14:
-	leave
-	.cfi_def_cfa 7, 8
+	callq	__cxx_global_var_init
+	popq	%rax
 	ret
+.Ltmp11:
+	.size	_GLOBAL__I_a, .Ltmp11-_GLOBAL__I_a
 	.cfi_endproc
-.LFE1338:
-	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
-	.type	_GLOBAL__sub_I__Z6qusortiiPi, @function
-_GLOBAL__sub_I__Z6qusortiiPi:
-.LFB1339:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$65535, %esi
-	movl	$1, %edi
-	call	_Z41__static_initialization_and_destruction_0ii
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE1339:
-	.size	_GLOBAL__sub_I__Z6qusortiiPi, .-_GLOBAL__sub_I__Z6qusortiiPi
-	.section	.init_array,"aw"
-	.align 8
-	.quad	_GLOBAL__sub_I__Z6qusortiiPi
-	.hidden	__dso_handle
-	.ident	"GCC: (Ubuntu/Linaro 4.8.1-10ubuntu8) 4.8.1"
-	.section	.note.GNU-stack,"",@progbits
+
+	.type	_ZStL8__ioinit,@object  # @_ZStL8__ioinit
+	.lcomm	_ZStL8__ioinit,1
+	.type	.L_ZZ4mainE1a,@object   # @_ZZ4mainE1a
+	.section	.rodata,"a",@progbits
+	.align	16
+.L_ZZ4mainE1a:
+	.long	14                      # 0xe
+	.long	2                       # 0x2
+	.long	56                      # 0x38
+	.long	8                       # 0x8
+	.long	6                       # 0x6
+	.size	.L_ZZ4mainE1a, 20
+
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	 " "
+	.size	.L.str, 2
+
+	.section	.ctors,"aw",@progbits
+	.align	8
+	.quad	_GLOBAL__I_a
+
+	.section	".note.GNU-stack","",@progbits
